7934b15770d64d6b28ada983672fc81afa18c17e
ddana0909 <dana.diac@yahoo.com>
2013-12-03 07:42:13 +0000

    basic DragAndDrop

--- Scripts/Engine.js
+++ Scripts/Engine.js
@@ -3,11 +3,12 @@
  */
 //<reference path="Scripts/modernizr-latest.js"/>
 
-function canvasInit(canvasname,aspectRatio)
+function canvasInit(canvasName,aspectRatio)
 {
-    var canvas = document.getElementById(canvasname);
+    var canvas = document.getElementById(canvasName);
     canvas.width = window.innerWidth;
     canvas.height = canvas.width/aspectRatio;
+    getCanvasContext("canvasOne").clearRect(0, 0, canvasOne.width, canvasOne.height);
 }
 
 function canvasSupport ()
@@ -18,26 +19,19 @@ function canvasSupport ()
 function getCanvasContext(name)
 {
     var theCanvas = document.getElementById(name);
-    var context = theCanvas.getContext("2d");
-    return context;
+    return theCanvas.getContext("2d");
 }
 
 function drawShapeArray(shapes)
 {
     for (var i = 0; i < shapes.length; i++)
-    {
         shapes[i].draw();
-    }
 }
+
 
 function distanceBetweenPoints(x1,y1,x2,y2)
 {
     return Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
-}
-
-function area(ax,ay,bx,by,cx,cy)
-{
-    return Math.abs(parseFloat(ax*(bx-cy)+bx*(cy-ay)+cx*(ay-by)))/2;
 }
 
 function Sound(source, reapeat)
@@ -57,9 +51,9 @@ function playSound()
 
 Sound.prototype.playSound=playSound;
 
-function Rectangle(canvasname,width, height, positionX, positionY,color,fillColor)
+function Rectangle(canvasName,width, height, positionX, positionY,color,fillColor)
 {
-    this.canvasname=canvasname;
+    this.canvasName=canvasName;
     this.width=width;
     this.height=height;
     this.positionX=positionX;
@@ -70,89 +64,104 @@ function Rectangle(canvasname,width, height, positionX, positionY,color,fillColo
 
 Rectangle.prototype.draw=drawRectangle;
 
-Rectangle.prototype.isPointInside=function(x,y)
+Rectangle.prototype.area=areaRectangle;
+
+Rectangle.prototype.isPointInside = function (x, y)
 {
-    if( x < this.positionX || x > this.positionX  + this.width)
-    {
+    if (x < this.positionX || x > this.positionX + this.width) {
         //alert("x"+x.toString()+" "+ y.toString()+" "+this.positionX.toString()+" "+(this.positionX+this.width).toString());
         return false;
     }
-    if( y < this.positionY || y > this.positionY + this.height)
-    {
+    if (y < this.positionY || y > this.positionY + this.height) {
         //alert("y"+x.toString()+" "+ y.toString()+" "+this.positionY.toString()+" "+(this.positionY+this.height).toString());
         return false;
     }
 
     return true;
-}
+};
 
 Rectangle.prototype.move=function(newX, newY)
 {
     this.positionX=newX;
     this.positionY=newY;
-}
+};
 
 function drawRectangle()
 {
-    var context=getCanvasContext(this.canvasname);
+    var context=getCanvasContext(this.canvasName);
+    context.beginPath();
+    context.strokeStyle = this.color;
+    context.lineWidth = 3;
+
     context.rect(this.positionX, this.positionY, this.width,this.height);
+
+    context.stroke();
     if(this.fillColor)
     {
         context.fillStyle=this.fillColor;
         context.fill();
     }
-    context.strokeStyle = this.color;
-    context.stroke();
+
+}
+
+function areaRectangle()
+{
+    return this.width*this.length;
 }
 
 
-function Circle(canvasname,positionX,positionY,radius,color,fillcolor)
+function Circle(canvasName,positionX,positionY,radius,color,fillColor)
 {
-    this.canvasname=canvasname;
+    this.canvasName=canvasName;
     this.positionX=positionX;
     this.positionY=positionY;
     this.radius=radius;
     this.color=color;
-    this.fillcolor=fillcolor;
+    this.fillColor=fillColor;
 }
+
 Circle.prototype.draw=drawCircle;
 
-Circle.prototype.isPointInside=function(x,y)
-{
-    if (distanceBetweenPoints(this.positionX, this.positionY, x, y) > this.radius)
-        return false;
-    return true;
-}
+Circle.prototype.area=areaCircle;
 
-Circle.prototype.move=function(newX, newY)
-{
-    this.positionX=newX;
-    this.positionY=newY;
-}
+Circle.prototype.isPointInside = function (x, y) {
+    return distanceBetweenPoints(this.positionX, this.positionY, x, y) <= this.radius;
+
+};
+
+Circle.prototype.move = function (newX, newY) {
+    this.positionX = newX;
+    this.positionY = newY;
+};
 
 function drawCircle()
 {
-    var context=getCanvasContext(this.canvasname);
+    var context=getCanvasContext(this.canvasName);
     context.beginPath();
     context.strokeStyle = this.color;
-    context.lineWidth = 5;
+    context.lineWidth = 3;
     context.arc(this.positionX, this.positionY, this.radius, 0, (Math.PI/180)*360, false);
     context.stroke();
     context.closePath();
-    if(this.fillcolor)
+    if(this.fillColor)
     {
-        context.fillStyle=this.fillcolor;
+        context.fillStyle=this.fillColor;
         context.fill();
     }
 }
 
-function Triangle(canvasname,positionX,positionY,height,basisLength,color, fillcolor)
+function areaCircle()
 {
-    this.canvasname=canvasname;
+    return Math.PI*Math.pow(this.radius,2);
+}
+
+function Triangle(canvasName,positionX,positionY,height,basisLength,color, fillColor)
+{
+    this.canvasName=canvasName;
     this.height=height;
     this.basisLength=basisLength;
     this.color=color;
-    this.fillcolor=fillcolor;
+    this.fillColor=fillColor;
     this.positionX=positionX;
     this.positionY=positionY;
     this.x2 =this.positionX -basisLength/2;
@@ -163,58 +172,51 @@ function Triangle(canvasname,positionX,positionY,height,basisLength,color, fillc
 
 Triangle.prototype.draw = drawTriangle;
 
-Triangle.prototype.isPointInside=function (x, y)
-{
-    /*var a1= area(this.x1,this.y1,this.x2,this.y2,x,y);
-     var a2=area(this.x2,this.y2,this.x3,this.y3,x,y);
-     var a3=area(this.x1,this.y1,this.x3,this.y3,x,y);
-     var a4= area(this.x1,this.y1,this.x2,this.y2,this.x3,this.y3);
-     alert(a1.toString()+" "+a2.toString()+" "+a3.toString()+" "+a4.toString());
-     if(a1+a2+a3==a4)*/
-    /*var s1= (this.x2-this.x1)*(this.y-this.y1)-(this.x-this.x1)*(this.y2-this.y1);
-     var s2= (this.x3-this.x2)*(this.y-this.y2)-(this.x-this.x2)*(this.y3-this.y2);
-     var s3= (this.x1-this.x2)*(this.y-this.y3)-(this.x-this.x3)*(this.y1-this.y3);
-     alert(s1.toString()+" "+s2.toString()+" "+s3.toString());
-     if(s1==s2&&s2==s3)*/
-    //barycentric coordinates
-    var lambda1=parseFloat((this.y2-this.y3)*(x-this.x3)+(this.x3-this.x2)*(y-this.y3))/((this.y2-this.y3)*(this.positionX-this.x3)+(this.x3-this.x2)*(this.positionY-this.y3))
-    var lambda2=parseFloat((this.y3-this.positionY)*(x-this.x3)+(this.positionX-this.x3)*(y-this.y3))/((this.y2-this.y3)*(this.positionX-this.x3)+(this.x3-this.x2)*(this.positionY-this.y3))
-    var lambda3=1-lambda1-lambda2;
-    if(lambda1>=0&&lambda1<=1&&lambda2>=0&&lambda2<=1&&lambda3>=0&&lambda3<=1)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+Triangle.prototype.area=areaTriangle;
 
-Triangle.prototype.move=function(newX,newY)
-{   var dx=this.positionX-newX;
-    var dy=this.positionY-newY;
-    this.positionX=newX;
-    this.positionY=newY;
-    this.x2 -=dx;
-    this.y2 -=dy;
-    this.x3 -=dx;
-    this.y3 -=dy;
-}
+Triangle.prototype.isPointInside = function (x, y) {
+
+    //barycentric coordinates
+    var lambda1 = parseFloat((this.y2 - this.y3) * (x - this.x3) + (this.x3 - this.x2) * (y - this.y3)) / ((this.y2 - this.y3) * (this.positionX - this.x3) + (this.x3 - this.x2) * (this.positionY - this.y3));
+    var lambda2 = parseFloat((this.y3 - this.positionY) * (x - this.x3) + (this.positionX - this.x3) * (y - this.y3)) / ((this.y2 - this.y3) * (this.positionX - this.x3) + (this.x3 - this.x2) * (this.positionY - this.y3));
+    var lambda3 = 1 - lambda1 - lambda2;
+
+    return lambda1 >= 0 && lambda1 <= 1 && lambda2 >= 0 && lambda2 <= 1 && lambda3 >= 0 && lambda3 <= 1;
+
+};
+
+Triangle.prototype.move = function (newX, newY) {
+    var dx = this.positionX - newX;
+    var dy = this.positionY - newY;
+    this.positionX = newX;
+    this.positionY = newY;
+    this.x2 -= dx;
+    this.y2 -= dy;
+    this.x3 -= dx;
+    this.y3 -= dy;
+};
 
 function drawTriangle()
 {
-    var context = getCanvasContext(this.canvasname);
+    var context = getCanvasContext(this.canvasName);
+    context.beginPath();
     context.strokeStyle = this.color;
+    context.lineWidth = 3;
     context.moveTo(this.positionX, this.positionY);
     context.lineTo(this.x2, this.y2);
     context.lineTo(this.x3, this.y3);
     context.lineTo(this.positionX, this.positionY);
     context.stroke();
-    if (this.fillcolor)
+    if (this.fillColor)
     {
-        context.fillStyle = this.fillcolor;
+        context.fillStyle = this.fillColor;
         context.fill();
     }
+}
+
+function areaTriangle()
+{
+    return (this.basisLength*this.height)/2;
 }
 
 function Picture(source, width, height)
@@ -224,51 +226,117 @@ function Picture(source, width, height)
     this.height=height;
 }
 
-function displayPicture(canvasname,source, width, height, gridColumns, gridRows, gridColumn, gridRow, gridMarginLeft, gridMarginTop)
-{   var context=getCanvasContext(canvasname);
-    var canvas=document.getElementById(canvasname);
-    gridMarginLeft=gridMarginLeft*canvas.width;
-    gridMarginTop=gridMarginTop*canvas.height;
+function adjustDimensionsForGrid(cellHeight, cellWidth, height, width)
+{
+    var imageRatio = width / height;
+    if (cellHeight < height) {
+        height = cellHeight;
+        width = imageRatio * height;
+    }
+    if (cellWidth < width) {
+        width = cellWidth;
+        height = width / imageRatio;
+    }
+    return {height: height, width: width};
+}
 
-    var availableWidth=canvas.width-2*gridMarginLeft;
-    var availableHeight=canvas.height-2*gridMarginTop;
-    var imageRatio=width/height;
+function getSettingsForGrid( canvasName,gridMarginLeft,gridMarginTop, gridColumns, gridRows, height, width, gridColumn, gridRow)
+{
+    var canvas = document.getElementById(canvasName);
+
+    gridMarginLeft = gridMarginLeft * canvas.width;
+    gridMarginTop = gridMarginTop * canvas.height;
+
+    var availableWidth = canvas.width - 2 * gridMarginLeft;
+    var availableHeight = canvas.height - 2 * gridMarginTop;
 
     //create the grid stuff
-    var cellWidth=availableWidth/parseFloat(gridColumns);
-    var cellHeight=availableHeight/parseFloat(gridRows);
+    var cellWidth = availableWidth / parseFloat(gridColumns);
+    var cellHeight = availableHeight / parseFloat(gridRows);
 
-    if(cellHeight<height)
-    {
-        height=cellHeight;
-        width=imageRatio*height;
-    }
-    if(cellWidth<width)
-    {
-        width=cellWidth;
-        height=width/imageRatio;
-    }
+    var __ret = adjustDimensionsForGrid(cellHeight, cellWidth, height, width);
+    height = __ret.height;
+    width = __ret.width;
 
-    var positionOnX=gridMarginLeft+(gridColumn-1)*cellWidth;
-    var positionOnY=gridMarginTop+(gridRow-1)*cellHeight;
+    var positionOnX = gridMarginLeft + (gridColumn - 1) * cellWidth;
+    var positionOnY = gridMarginTop + (gridRow - 1) * cellHeight;
 
     //center in cell on OX
 
-    var d=(cellWidth-width)/2;
-    positionOnX=positionOnX+d;
+    var d = (cellWidth - width) / 2;
+    positionOnX = positionOnX + d;
 
-    //display image
+    //center in cell on OY
 
-    var imageToDisplay = new Image();
-    imageToDisplay.src = source;
-    imageToDisplay.id=source;
-    imageToDisplay.onload = function ()
-    {
-        context.drawImage(imageToDisplay, positionOnX,positionOnY,width,height);
-    }
+    var dy=(cellHeight-height)/2;
+    positionOnY=positionOnY+dy;
 
+    return {height: height, width: width, positionOnX: positionOnX, positionOnY: positionOnY};
 }
 
+function displayPicture(canvasName,source, width, height, gridColumns, gridRows, gridColumn, gridRow, gridMarginLeft, gridMarginTop)
+{
+    var context = getCanvasContext(canvasName);
 
+    var __ret = getSettingsForGrid(canvasName, gridMarginLeft, gridMarginTop, gridColumns, gridRows, height, width, gridColumn, gridRow);
+    height = __ret.height;
+    width = __ret.width;
+    var positionOnX = __ret.positionOnX;
+    var positionOnY = __ret.positionOnY;
+
+    //display imag
+    var imageToDisplay = new Image();
+    imageToDisplay.src = source;
+    imageToDisplay.id = source;
+    imageToDisplay.onload = function ()
+    {
+        context.drawImage(imageToDisplay, positionOnX, positionOnY, width, height);
+    }
+}
+
+function getShapeModel(shapeModel)
+{
+    var settings = getSettingsForGrid("canvasOne",0.05,0.01,3,3,150,150,2,2);
+    var square=new Rectangle("canvasOne",settings.width,settings.height,settings.positionOnX, settings.positionOnY,"black","#FBB829");
+    shapeModel.push(square);
+
+    var cSettings=getSettingsForGrid("canvasOne",0.05,0.01,3,3,50,50,2,2);
+    cSettings.width=settings.width/3;
+    cSettings.positionOnX=settings.positionOnX+settings.width/2;
+    cSettings.positionOnY=settings.positionOnY+settings.width/2;
+    var circle= new Circle("canvasOne",cSettings.positionOnX,cSettings.positionOnY,cSettings.width/2,"black","#FBB829");
+    shapeModel.push(circle);
+
+
+    var tSettings=getSettingsForGrid("canvasOne",0.05,0.01,3,3,40,40,2,2);
+    tSettings.width=0.8*settings.width/3;
+    tSettings.height=0.8*settings.height/3;
+    var triangle= new Triangle("canvasOne",cSettings.positionOnX ,cSettings.positionOnY-tSettings.height-cSettings.width/2-3,tSettings.height,tSettings.width,"black","#FBB829");
+    shapeModel.push(triangle);
+
+    var rvSettings=getSettingsForGrid("canvasOne",0.05,0.01,3,3,80,50,2,2);
+    rvSettings.width=settings.width/3;
+    rvSettings.height=settings.height/2;
+    rvSettings.positionOnY-=(rvSettings.height+(rvSettings.positionOnY-settings.positionOnY+3));
+    rvSettings.positionOnX=settings.positionOnX+rvSettings.width;
+    var vrectangle=new Rectangle("canvasOne",rvSettings.width,rvSettings.height,rvSettings.positionOnX, rvSettings.positionOnY,"black","#FBB829");
+    shapeModel.push(vrectangle);
+}
+var myClasses=[];
+myClasses["Rectangle"]=Rectangle;
+myClasses["Circle"]=Circle;
+myClasses["Triangle"]=Triangle;
+
+function getObjectType(obj)
+{
+    for(var oType in myClasses)
+        if(obj instanceof myClasses[oType])
+            return oType;
+}
+
+function clearArray(name)
+{
+    name.splice(0,name.length);
+}
 
 
--- Scripts/ShapeGame.js
+++ Scripts/ShapeGame.js
@@ -5,7 +5,7 @@
 //<reference path="Scripts/Engine.js"/>
 
 window.addEventListener("load", eventWindowLoaded, false);
-function eventWindowLoaded(event)
+function eventWindowLoaded()
 {   playInstructions();
     var button=document.getElementById("instructions");
     button.addEventListener("click", playInstructions,false);
@@ -13,40 +13,58 @@ function eventWindowLoaded(event)
 }
 window.addEventListener("resize", OnResize, false);
 
-function OnResize(event)
-{   canvasInit("canvasOne",2.5);
+function OnResize()
+{
+    canvasInit("canvasOne",2.5);
+    clearArray(shapes);
+    clearArray(shapeModel);
     canvasApp();
 }
 
-var shapes=new Array();
+var shapes=[];
 var dx,dy;
-var shapeIndex;
-
-
+//var shapeIndex;
+var shapeModel=[];
 
 
 function canvasApp()
 {   if(!canvasSupport())
         return;
     canvasInit("canvasOne",2.5);
-    var rectangle=new Rectangle("canvasOne",50,50,20,20,"black","#FBB829");
-    shapes.push(rectangle);
+    getShapeModel(shapeModel);
 
-    var circle= new Circle("canvasOne",100,100,20,"black","#FBB829");
+    var settings = getSettingsForGrid("canvasOne",0.05,0.01,15,7,50,50,4,7);
+    var square=new Rectangle("canvasOne",settings.width,settings.height,settings.positionOnX, settings.positionOnY,"black","#8A9B0F");
+    shapes.push(square);
+
+    var cSettings=getSettingsForGrid("canvasOne",0.05,0.01,15,7,50,50,6,7);
+    var circle= new Circle("canvasOne",cSettings.positionOnX+cSettings.width/2,cSettings.positionOnY+cSettings.width/2,cSettings.width/2,"black","#8A9B0F");
     shapes.push(circle);
 
-    var triangle= new Triangle("canvasOne",200,200,50,80,"black","#FBB829");
+
+    var tSettings=getSettingsForGrid("canvasOne",0.05,0.01,15,7,50,50,8,7);
+    var triangle= new Triangle("canvasOne",tSettings.positionOnX, tSettings.positionOnY,tSettings.height,tSettings.width,"black","#8A9B0F");
     shapes.push(triangle);
+
+    var rSettings=getSettingsForGrid("canvasOne",0.05,0.01,15,7,50,80,10,7);
+    var rectangle=new Rectangle("canvasOne",rSettings.width,rSettings.height,rSettings.positionOnX, rSettings.positionOnY,"black","#8A9B0F");
+    shapes.push(rectangle);
+
+    var rvSettings=getSettingsForGrid("canvasOne",0.05,0.01,15,7,80,50,12,7);
+    var vrectangle=new Rectangle("canvasOne",rvSettings.width,rvSettings.height,rvSettings.positionOnX, rvSettings.positionOnY,"black","#8A9B0F");
+    shapes.push(vrectangle);
 
     drawShapeArray(shapes);
 
-    displayPicture("canvasOne","Images/lionface.png",792,1009,15,4,1,4,0.05,0.05);
+    drawShapeArray(shapeModel);
+
+    displayPicture("canvasOne","Images/lionface.png",792,1009,15,4,1,4,0.01,0.01);
 
     canvasOne.addEventListener("mousedown",mouseDownEvent,false);
 }
 
 function mouseDownEvent(event)
-{
+{   var shapeIndex;
     var hit=false;
     for(var i=0;i<shapes.length;i++)
     {
@@ -58,13 +76,13 @@ function mouseDownEvent(event)
     }
     if(hit)
     {  canvasOne.addEventListener("mousemove",mouseMoveEvent,false);
-
-        /*var selectedShape=shapes.slice(shapeIndex,shapeIndex+1); //select
+        var selectedShape=shapes[shapeIndex]; //select
         shapes.splice(shapeIndex,1);                            //delete
         shapes.unshift(selectedShape);
-        */
-        dx=shapes[shapeIndex].positionX-event.pageX;
-        dy=shapes[shapeIndex].positionY-event.pageY;
+
+
+        dx=shapes[0].positionX-event.pageX;
+        dy=shapes[0].positionY-event.pageY;
 
     }
 
@@ -74,10 +92,10 @@ function mouseDownEvent(event)
 
 function mouseMoveEvent(event)
     {
-        var newPositionX=event.pageX+dx;
-        var newPositionY=event.pageY+dy;
+        //var newPositionX=event.pageX+dx;
+        //var newPositionY=event.pageY+dy;
 
-        shapes[shapeIndex].move(newPositionX,newPositionY);
+        //shapes[0].move(newPositionX,newPositionY);
 
         canvasOne.addEventListener("mouseup",mouseUpEvent,false);
     }
@@ -85,10 +103,52 @@ function mouseMoveEvent(event)
 function mouseUpEvent(event)
     {
         canvasOne.removeEventListener("mousemove",mouseMoveEvent,false);
-        var context=getCanvasContext("canvasOne");
-        context.clearRect(0, 0, canvasOne.width, canvasOne.height);
-        displayPicture("canvasOne","Images/lionface.png",792,1009,15,4,1,4,0.05,0.05);
-        drawShapeArray(shapes);
+
+        var nrMatch=0;
+        var match=false;
+        var shapeIndex=[];
+        var indexOfMin=0;
+
+        for(var i=0;i<shapeModel.length;i++)
+        {
+            if(shapeModel[i].isPointInside(event.pageX,event.pageY))
+            {
+                nrMatch++;
+                shapeIndex.push(i);
+            }
+        }
+
+        if(nrMatch>1)
+        {
+           var minSize=999*999;
+
+           for( var j in shapeIndex)
+            {  var indexInShapeModel=shapeIndex[j];
+                if (shapeModel[indexInShapeModel].area() < minSize)
+                {
+                    minSize = shapeModel[indexInShapeModel].area();
+                    indexOfMin = indexInShapeModel;
+                }
+            }
+           if(getObjectType(shapeModel[indexOfMin])==getObjectType(shapes[0]))
+                match=true;
+           else
+                 alert("bai! ce vrei sa faci?")
+        }
+        if(nrMatch==1)
+            if(getObjectType(shapeModel[shapeIndex[0]])==getObjectType(shapes[0]))
+            {match=true;
+             indexOfMin=shapeIndex[0];
+            }
+
+        //context.clearRect(0, 0, canvasOne.width, canvasOne.height);
+        //displayPicture("canvasOne","Images/lionface.png",792,1009,15,4,1,4,0.01,0.01);
+        if(match)
+        {
+        alert('GOOD!');
+        shapeModel[indexOfMin].fillColor=shapes[0].fillColor;
+        drawShapeArray(shapeModel);
+        }
     }
 
 function playSound() {
